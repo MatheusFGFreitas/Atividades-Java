10b1fbffa29b90e2fa45f2131b9a253664c406ae
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-10-09 18:27:51 -0300

    herança multipla e o problema do diamante parte 1

---
+++ interfaces/src/model/entities/ComboDevice.java
@@ -0,0 +1,6 @@
+package model.entities;
+
+public class ComboDevice extends Scanner, printer {//não pode mandar uma classe extender mais que uma classe, ou é scanner ou o printer
+	
+	public ComboDevice
+}
