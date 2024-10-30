fdfc58a0f975bd2a88e7ab44cabab7a012a64fd4
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-10-30 16:51:01 -0300

    resolução dos erros em get/put

--- generics_set_map/src/application/PrincipioGetPut.java
+++ generics_set_map/src/application/PrincipioGetPut.java
@@ -1,33 +1,39 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrincipioGetPut {
 
 	public static void main(String[] args) {
 		
-		//covariancia
-		List<Integer> intList = new ArrayList<Integer>();
-		intList.add(10);
-		intList.add(5);
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
 		
-		List<? extends Number> list = intList;
-		Number x = list.get(0);
-		list.add(20); // erro de compilacao na hora de adicionar, mas acessar não tem problema
-		//o ? extends Number deixa a lista bloqueada a novas adições, pois o sistema pode não saber qual é o tipo que a lista aceita
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		
-		//contravariancia
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
 		
-		List<? super Number> myNums = myObjs;
-		myNums.add(10);
-		myNums.add(3.14);
-		Number x = myNums.get(0); // erro de compilacao na hora de acessar, mas adicionar não tem problema
-		//a atribuição no acesso não é feita pois pode ser uma variavel diferente na lista do que lista o myNums
+		copy(myInts, myObjs);
+		printList(myObjs);
+		
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 		
 	}
-
+	
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {//graças ao (? extends Number> a lista pode conter qualquer tipo que estenda number, como integer, double etc...
+																						  //graças ao <? super Number> a lista de destino pode aceitar qualquer supertipo de number o que significa que pode ser number até object
+																						  //resumindo, esse metodo permite copiar os elementos(ou seus filhos) e copiar para uma lista seus numeros(ou super tipos)
+		for(Number number : source) {
+			destiny.add(number);
+		}
+	}
+	
+	public static void printList(List<?> list) {
+		for( Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
+	}
 }
