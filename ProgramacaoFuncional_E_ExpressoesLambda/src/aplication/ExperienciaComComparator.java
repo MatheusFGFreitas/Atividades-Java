e84b3fc89b78820ad9a6497bb25b2bb695d4f51c
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-11-06 14:51:23 -0300

    Comparator, com exemplos e função lambda

--- ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/ExperienciaComComparator.java
+++ ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/ExperienciaComComparator.java
@@ -1,6 +1,7 @@
 package aplication;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entitites.Product;
@@ -15,7 +16,25 @@ public class ExperienciaComComparator {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		list.sort(new MyComparator());
+		//Comparator<Product> comp = new Comparator<Product>() {//feito no mesmo programa, porém mt longo
+			//@Override
+			//public int compare(Product p1, Product p2) {
+				//return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			//}
+			
+		//};
+		
+		Comparator<Product> comp = (p1, p2) ->{//definindo uma função anonima, com essa sintaxe também chamada de arrow function
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());//feito em lambda
+		};
+		
+		//também pode ser feito desse jeito mais enxuto e o codigo em uma só linha
+		//Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		
+		//também pode colocar a expressão lambda dentro do list sort, desse jeito
+		//list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		list.sort(comp);
 		
 		for (Product p : list) {
 			System.out.println(p);
