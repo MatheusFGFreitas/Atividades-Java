a8400b880420876d72f1249d8dfa5fbfd497d7ef
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-11-07 13:02:54 -0300

    Interfaces funcionais

--- ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/MyComparator.java
+++ ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/MyComparator.java
@@ -4,11 +4,12 @@ import java.util.Comparator;
 
 import entitites.Product;
 
-public class MyComparator implements Comparator<Product>{
+public class MyComparator implements Comparator<Product>{//interface funcional é a interface que possui um unico metodo abstrato
 
 	@Override
-	public int compare(Product p1, Product p2) {
+	public int compare(Product p1, Product p2) {//isso também é uma interface funcional, suas implementações são tratadas como expressão lambda
 		return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-	}
+	}//existe varias interfaces funcionais, por exemplo predicatem function e consumer(consumer é esperado que possa gerar efeitos colaterais)
+	
 
 }
