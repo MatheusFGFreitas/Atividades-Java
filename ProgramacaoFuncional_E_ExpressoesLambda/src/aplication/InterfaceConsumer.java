2e70ab9980793bf105b3bdc5a36d0a75094245ef
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-11-07 15:34:27 -0300

    Consumer Expressão lambda declarada

--- ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/InterfaceConsumer.java
+++ ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/InterfaceConsumer.java
@@ -3,9 +3,9 @@ package aplication;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entitites.Product;
-import util.PriceUpdate;
 
 public class InterfaceConsumer {
 
@@ -23,11 +23,19 @@ public class InterfaceConsumer {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		double factor = 1.1;//criada uma variavel que pode ser uma variavel lida pelo usuario, para não ter que ficar alterando manualmente
+		Consumer<Product> cons = p -> {//feito uma expressão lambda declarada 
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		
 		//list.forEach(new PriceUpdate());//o for each é um defauft method que ira percorrer a coleção e ira executar um consumer para cada elemento
 		
 		//list.forEach(Product::staticPriceUpdate);//referencia static no proprio product
 		
-		list.forEach(Product::nonStaticPriceUpdate);//referencia não estatica no proprio product
+		//list.forEach(Product::nonStaticPriceUpdate);//referencia não estatica no proprio product
+		
+		list.forEach(cons);//chamando a variavel com a expressão lambda
 		
 		list.forEach(System.out::println);//tenho que fazer um for each para printar cada um dos produtos na lista, o reference method
 
