2ba31acd55238949c32ed62c3ff4a9e60d983f76
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-11-21 16:22:00 -0300

    criada stream pelo Stream.of

--- ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/Streams.java
+++ ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/Streams.java
@@ -11,8 +11,10 @@ public class Streams {
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);//feito uma lista de inteiros
 		
 		Stream<Integer> st1 = list.stream().map(x -> x * 10);//criada uma stream de inteiros a partir da lista//agora feito com map para multiplicar tudo por 10
-		
 		System.out.println(Arrays.toString(st1.toArray()));//chamado to array para converter a stream para um vetor
+		
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");//criada uma stream of com os elementos já dentro da stream
+		System.out.println(Arrays.toString(st2.toArray()));
 
 	}
 
