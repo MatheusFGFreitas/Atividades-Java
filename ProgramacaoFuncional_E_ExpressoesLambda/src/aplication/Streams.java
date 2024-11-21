dfda58386eec12647208845fcd66f20d59d1987d
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2024-11-21 16:18:20 -0300

    Stream com map

--- ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/Streams.java
+++ ProgramacaoFuncional_E_ExpressoesLambda/src/aplication/Streams.java
@@ -10,7 +10,7 @@ public class Streams {
 		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);//feito uma lista de inteiros
 		
-		Stream<Integer> st1 = list.stream();//criada uma stream de inteiros a partir da lista
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);//criada uma stream de inteiros a partir da lista//agora feito com map para multiplicar tudo por 10
 		
 		System.out.println(Arrays.toString(st1.toArray()));//chamado to array para converter a stream para um vetor
 
