91304174156f88f1f93b3062b11c9ea9307f74a6
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2025-08-15 15:16:37 -0300

    Exercicio while 01!

---
+++ Estrutura_Repetitiva/src/Program/ExercicioWhile01.java
@@ -0,0 +1,24 @@
+package Program;
+
+import java.util.Scanner;
+
+public class ExercicioWhile01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		while (x != 2002) {
+			System.out.println("Senha invalida");
+			x = sc.nextInt();
+		}
+		
+		System.out.println("Acesso permitido");
+		
+		sc.close();
+
+	}
+
+}
