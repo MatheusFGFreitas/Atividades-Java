e721fe2365d0d2eb00d863b01ae9cd4cb4a2a22f
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2025-08-20 13:45:26 -0300

    Exercicio For 02

---
+++ Estrutura_Repetitiva/src/Program/ExercicioFor02.java
@@ -0,0 +1,38 @@
+package Program;
+
+import java.util.Scanner;
+
+public class ExercicioFor02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int min, max, soma;
+		
+		soma = 0;
+		
+		if(x < y) {
+			min = x;
+			max = y;
+		}else {
+			min = y;
+			max = x;
+		}
+		
+		for(int i = min + 1; i < max; i++) {
+			if (i % 2 != 0) {
+				soma = soma + i;
+			}
+		}
+		
+		System.out.println(soma);
+		
+		sc.close();
+
+	}
+
+}
