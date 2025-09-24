12d48fdd3d3b04676c76cdd431870826c6f4bfcf
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2025-09-24 14:36:42 -0300

    Topicos basicos sobre java operadores bit a bit

---
+++ TopicosBasicosJava/src/Program/OperadoresBitABit.java
@@ -0,0 +1,16 @@
+package Program;
+
+public class OperadoresBitABit {
+
+	public static void main(String[] args) {
+		
+		int n1 = 89;
+		int n2 = 60;
+		
+		System.out.println(n1 & n2);//Operação "E" bit a bit
+		System.out.println(n1 | n2);//Operação "OU" bit a bit
+		System.out.println(n1 ^ n2);//Operação "OU-exclusivo" bit a bit
+
+	}
+
+}
