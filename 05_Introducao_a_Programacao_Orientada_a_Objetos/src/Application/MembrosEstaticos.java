181e9905be9ec5a918592cdb284047a21575e74d
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2025-10-20 20:00:54 -0300

    Membros estaticos parte 2

--- 05_Introducao_a_Programacao_Orientada_a_Objetos/src/Application/MembrosEstaticos.java
+++ 05_Introducao_a_Programacao_Orientada_a_Objetos/src/Application/MembrosEstaticos.java
@@ -3,37 +3,40 @@ package Application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import Entities.Calculador;
+
 public class MembrosEstaticos {
 	
-	public static final double PI = 3.14159;// inserido um membro estatico, o final é para indicar que a variavel PI será constante, ou seja, não sera alterado
+	//public static final double PI = 3.14159;// inserido um membro estatico, o final é para indicar que a variavel PI será constante, ou seja, não sera alterado
 											// e o padrao de nomes para constante é sempre maiuscula, com um _ pra separar a palavra: NET_SALARY
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Calculador calc = new Calculador();//instanciado um objeto na classe
 		
 		System.out.print("Entre o raio: ");
 		double raio = sc.nextDouble();
 		
-		double c = circunferencia(raio);
+		double c = calc.circunferencia(raio);
 		
-		double v = volume(raio);
+		double v = calc.volume(raio);
 		
 		System.out.printf("Circunferencia: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("Valor de PI: %.2f%n", PI);
+		System.out.printf("Valor de PI: %.2f%n", calc.PI);
 		
 		
 		sc.close();
 
 	}
 	
-	public static double circunferencia(double raio) {
-		return 2.0 * PI * raio;
-	}
+	//public static double circunferencia(double raio) {//criado um membro estatico depois do fim do programa
+		//return 2.0 * PI * raio;
+	//}
 	
-	public static double volume(double raio) {
-		return 4.0 * PI * raio * raio * raio / 3.0;
-	}
+	//public static double volume(double raio) {
+		//return 4.0 * PI * raio * raio * raio / 3.0;
+	//}
 
 }
--- 05_Introducao_a_Programacao_Orientada_a_Objetos/src/Entities/Calculador.java
+++ 05_Introducao_a_Programacao_Orientada_a_Objetos/src/Entities/Calculador.java
@@ -1,5 +1,17 @@
 package Entities;
 
 public class Calculador {
+	
+	//argumentos movidos do membro estatico para o calculador, para delegar as funçoes
+	
+	public final double PI = 3.14159;
+	
+	public double circunferencia(double raio) {
+		return 2.0 * PI * raio;
+	}
+	
+	public double volume(double raio) {
+		return 4.0 * PI * raio * raio * raio / 3.0;
+	}
 
 }
