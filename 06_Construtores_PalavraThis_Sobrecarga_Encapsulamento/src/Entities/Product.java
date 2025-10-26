3f58adf3b7bba26d4e5a7a27c2e94050ec6b26d7
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2025-10-26 11:33:19 -0300

    Construtores

---
+++ 06_Construtores_PalavraThis_Sobrecarga_Encapsulamento/src/Entities/Product.java
@@ -0,0 +1,36 @@
+package Entities;
+
+public class Product {
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Product(String name, double price, int quantity) {//obrigando o usuario a instanciar o nome preço e quantidade para não ficar nulo
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public double totalValueInStock() {
+	return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+	this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+	this.quantity -= quantity;
+	}
+	
+	public String toString() {
+	return name
+	+ ", $ "
+	+ String.format("%.2f", price)
+	+ ", "
+	+ quantity
+	+ " units, Total: $ "
+	+ String.format("%.2f", totalValueInStock());
+	}
+}
